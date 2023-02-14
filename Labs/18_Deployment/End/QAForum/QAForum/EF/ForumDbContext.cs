a971a2bc21481757c6f8dd28370663a2fbe1b304
Philjh <phil.howarth@btinternet.com>
2023-02-14 13:51:03 +0000

    Added Lab files

---
+++ Labs/18_Deployment/End/QAForum/QAForum/EF/ForumDbContext.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Proxies;
+using QAForum.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QAForum.EF
+{
+    public class ForumDbContext : DbContext
+    {
+        public ForumDbContext(DbContextOptions<ForumDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Forum>? Forums { get; set; }
+        public DbSet<QAForum.Models.Thread>? Threads { get; set; }
+        public DbSet<Post>? Posts { get; set; }
+        public DbSet<UnhandledException>? UnhandledExceptions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<QAForum.Models.Thread>()
+                .HasOne(t => t.Forum)
+                .WithMany(f => f.Threads)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.Thread)
+                .WithMany(t => t.Posts)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Forum>().HasData
+                                        (
+                                            new Forum { ForumId = 1, Title = "ASP.NET MVC" },
+                                            new Forum { ForumId = 2, Title = "ASP.NET Blazor" },
+                                            new Forum { ForumId = 3, Title = "ASP.NET WebForms" },
+                                            new Forum { ForumId = 4, Title = "jQuery" },
+                                            new Forum { ForumId = 5, Title = "Angular" },
+                                            new Forum { ForumId = 6, Title = "Visual Studio 2022" },
+                                            new Forum { ForumId = 7, Title = "WPF" }
+                                        );
+
+        }
+    }
+
+    public class ForumContextFactory : IDesignTimeDbContextFactory<ForumDbContext>
+    {
+        public ForumDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ForumDbContext>();
+            string conn = @"Server=.\SqlExpress;Database=Forum.Data;Trusted_Connection=true; MultipleActiveResultSets=True";
+            optionsBuilder.UseSqlServer(conn);
+
+            return new ForumDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Labs/18_Deployment/End/QAForum/QAForum/Migrations/20230209125802_Initial.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QAForum.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Forums",
+                columns: table => new
+                {
+                    ForumId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Forums", x => x.ForumId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UnhandledExceptions",
+                columns: table => new
+                {
+                    UnhandledExceptionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ExceptionDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExceptionMessage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BaseExceptionMessage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StackTrace = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnhandledExceptions", x => x.UnhandledExceptionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Threads",
+                columns: table => new
+                {
+                    ThreadId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ForumId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Threads", x => x.ThreadId);
+                    table.ForeignKey(
+                        name: "FK_Threads_Forums_ForumId",
+                        column: x => x.ForumId,
+                        principalTable: "Forums",
+                        principalColumn: "ForumId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ThreadId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostBody = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostDateTime = table.Column<DateTime>(type: "smalldatetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Threads_ThreadId",
+                        column: x => x.ThreadId,
+                        principalTable: "Threads",
+                        principalColumn: "ThreadId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Forums",
+                columns: new[] { "ForumId", "Title" },
+                values: new object[,]
+                {
+                    { 1, "ASP.NET MVC" },
+                    { 2, "ASP.NET Blazor" },
+                    { 3, "ASP.NET WebForms" },
+                    { 4, "jQuery" },
+                    { 5, "Angular" },
+                    { 6, "Visual Studio 2022" },
+                    { 7, "WPF" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_ThreadId",
+                table: "Posts",
+                column: "ThreadId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Threads_ForumId",
+                table: "Threads",
+                column: "ForumId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "UnhandledExceptions");
+
+            migrationBuilder.DropTable(
+                name: "Threads");
+
+            migrationBuilder.DropTable(
+                name: "Forums");
+        }
+    }
+}
